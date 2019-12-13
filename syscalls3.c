18d6a874db173aceaad0baceb7271f2eb232eb64
Manohar Vemuri <29159385+mvemuri6642@users.noreply.github.com>
2019-12-13 11:57:35 +0530

    Add files via upload

---
+++ syscalls3.c
@@ -0,0 +1,30 @@
+#include<sys/types.h>
+#include<unistd.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+void main(int argc,char *argv[])
+{
+int fd1,fd2,fd3,n,i,buf[100],buf2[100],buf3[100];
+fd1=open(argv[1],O_RDONLY);
+fd2=open(argv[2],O_WRONLY);
+fd3=open(argv[3],O_WRONLY);
+n=read(fd1,buf,sizeof(buf));
+for(i=0;i<n;i++)
+{
+if(buf[i]%2==0)
+{
+buf2[i]=buf[i];
+}
+else
+{
+buf3[i]=buf[i];
+
+}
+write(fd2,buf2,n);
+write(fd3,buf3,n);
+}
+close(fd1);
+close(fd2);
+close(fd3);
+}
+
