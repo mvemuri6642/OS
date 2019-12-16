064e6f24a24b6b8a97b910ff287796a0c2d1270c
Manohar Vemuri <29159385+mvemuri6642@users.noreply.github.com>
2019-12-16 11:41:43 +0530

    Update syscalls3.c

--- syscalls3.c
+++ syscalls3.c
@@ -4,27 +4,24 @@
 #include<fcntl.h>
 void main(int argc,char *argv[])
 {
-int fd1,fd2,fd3,n,i,buf[100],buf2[100],buf3[100];
-fd1=open(argv[1],O_RDONLY);
-fd2=open(argv[2],O_WRONLY);
-fd3=open(argv[3],O_WRONLY);
-n=read(fd1,buf,sizeof(buf));
-for(i=0;i<n;i++)
-{
-if(buf[i]%2==0)
-{
-buf2[i]=buf[i];
-}
-else
-{
-buf3[i]=buf[i];
-
-}
-write(fd2,buf2,n);
-write(fd3,buf3,n);
-}
-close(fd1);
-close(fd2);
-close(fd3);
+  int fd1,fd2,fd3,n,i,buf[100];
+  fd1=open(argv[1],O_RDONLY);
+  fd2=open(argv[2],O_WRONLY);
+  fd3=open(argv[3],O_WRONLY);
+  n=read(fd1,buf,sizeof(buf));
+  for(i=0;i<n;i++)
+  {
+    if(buf[i]%2==0)
+    {
+      write(fd2,buf,n);
+    }
+    else
+    {
+      write(fd3,buf,n);
+    }
+  }
+  close(fd1);
+  close(fd2);
+  close(fd3);
 }
 
